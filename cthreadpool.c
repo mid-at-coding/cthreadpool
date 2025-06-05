bdb09562f88adbb013027e4c5c4b4b16427131d6
ember <cattodoameow@proton.me>
2025-06-05 13:06:11 -0400

    oops

--- cthreadpool.c
+++ cthreadpool.c
@@ -4,7 +4,7 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-#include "../inc/cthreadpool.h"
+#include "cthreadpool.h"
 
 /* ======================== STATIC FUNCTIONS DEFINED ========================= */
 
